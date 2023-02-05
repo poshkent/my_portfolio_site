4ce6ec47b80da9eb65cc2ed72749e7918317bc8a
poshkent <poshkent@users.noreply.github.com>
2023-02-05 12:52:31 +0100

    Config for azure

--- portfolio_azure/settings.py
+++ portfolio_azure/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hn12dq32=d(5bzy=3h7w=@=5i$aw4jt6b)$*8vs+t-r@eq6lo^'
+IN_WEB = "SECRET_KEY" in os.environ
+if IN_WEB:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    DEBUG = False
+else:
+    DEBUG = True
+    from config import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
