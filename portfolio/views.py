d683076ad7c879d50824f687e409ad6a59f536ca
poshkent <poshkent@users.noreply.github.com>
2023-02-05 19:34:29 +0100

    add page and ico

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'portfolio/main.html', context=None)
+
+def pictures(request):
+    return render(request, 'portfolio/pictures.html', context=None)
--- portfolio_azure/urls.py
+++ portfolio_azure/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 
 
