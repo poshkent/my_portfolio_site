8d6f558426c04394a7f46a7d1e1a8de4424dc7cc
poshkent <poshkent@users.noreply.github.com>
2023-02-06 23:23:05 +0100

    added user page

--- users/urls.py
+++ users/urls.py
@@ -5,15 +5,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path(
-      'logout/',
-      # Прямо в описании обработчика укажем шаблон, 
-      # который должен применяться для отображения возвращаемой страницы.
-      # Да, во view-классах так можно! Как их не полюбить.
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
     path('', include('django.contrib.auth.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    path("account/", views.account, name="account"),
 ] 
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -9,3 +11,16 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def account(request):
+    if (request.method == "POST"):
+        user = User.objects.get(username=request.user)
+        if (request.POST.get('email') != user.email):
+            user.email = request.POST.get('email')
+        if (request.POST.get('first_name') != user.first_name):
+            user.first_name = request.POST.get('first_name')
+        if (request.POST.get('last_name') != user.last_name):
+            user.last_name = request.POST.get('last_name')
+        user.save()
+        return redirect('/auth/account')
+    return render(request, "users/account.html")
